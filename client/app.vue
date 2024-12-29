e83f4eb8ca4c7e9bde9b95dc01ed28ade976913e
alsey89 <phyokyawsoe89@gmail.com>
2024-12-29 16:09:40 +0800

    feat(client): set up client with tailwind, shadcn, pinia, nuxt

---
+++ client/app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="bg-red-500 p-8">
+    <Button class="bg-green-500 p-2">
+      <span>Click me</span>
+    </Button>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
